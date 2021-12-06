3ec7b877e6865bb51a12837785d5cb33f06a4838
dom-astro <94188743+dom-astro@users.noreply.github.com>
2021-12-06 21:13:38 +0100

    Ajout initial

---
+++ exoplanets.bat
@@ -0,0 +1,7 @@
+del C:\Users\t0050646\myApp\laPoste\logstash-7.0.1\data\plugins\inputs\file\.sincedb*
+
+cd C:\Users\t0050646\myApp\laPoste\logstash-7.0.1
+
+.\bin\logstash.bat -f C:\Users\t0050646\myApp\exoplanets\exoplanets.conf
+
+pause
