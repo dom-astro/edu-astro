054d0594f89349ce62b67a7295299b6f7fde361b
Dom Astro <94188743+dom-astro@users.noreply.github.com>
2022-01-04 16:01:10 +0100

    Delete exoplanets.bat

--- exoplanets.bat
+++
@@ -1,7 +0,0 @@
-del C:\Users\t0050646\myApp\laPoste\logstash-7.0.1\data\plugins\inputs\file\.sincedb*
-
-cd C:\Users\t0050646\myApp\laPoste\logstash-7.0.1
-
-.\bin\logstash.bat -f C:\Users\t0050646\myApp\exoplanets\exoplanets.conf
-
-pause
